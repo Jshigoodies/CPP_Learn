1107a93e2499fc6a360b4a8e2f98c2a4ef36c0ad
unknown <jshigoodies@gmail.com>
2021-12-21 23:59:33 -0600

    Learning

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+    string test = "hi";
     cout << "Hello world!\n";
     cout << "Next Line!" << endl;
     // endl makes a next line
@@ -35,5 +36,22 @@ int main()
     cin >> m;
 
     cout << m;
+
+    float f1 = 35e3;
+    double d1 = 12E4;
+    cout << f1;
+    cout << d1 << endl;
+
+
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun << endl;  // Outputs 1 (true)
+    cout << isFishTasty << endl;  // Outputs 0 (false)
+
+    char a = 65, b = 66, c = 67;
+    cout << a << endl; //ascii values
+    cout << b << endl;
+    cout << c << endl;
+
     return 0;
 }
